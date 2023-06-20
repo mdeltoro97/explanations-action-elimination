2c5a468506f92da6faee77eb86f5ce20def2190a
mdeltoro97 <mdeltoro@pa.uc3m.es>
2023-06-20 19:20:15 +0200

    modifying causal links method

--- src/translate/action-elimination.sas
+++ src/translate/action-elimination.sas
@@ -4,62 +4,175 @@ end_version
 begin_metric
 1
 end_metric
-2
+8
 begin_variable
 var0
 -1
-3
-Atom available-stick(stick1)
-Atom has-stick(stick1)
-Atom has_axe()
+2
+Atom clear(b)
+Atom irrelevant-fact()
 end_variable
 begin_variable
 var1
 -1
+2
+Atom clear(d)
+Atom irrelevant-fact()
+end_variable
+begin_variable
+var2
+-1
 3
+Atom holding(c)
+Atom ontable(c)
+Atom irrelevant-fact()
+end_variable
+begin_variable
+var3
+-1
+2
+Atom clear(c)
+Atom irrelevant-fact()
+end_variable
+begin_variable
+var4
+-1
+2
+Atom handempty()
+Atom irrelevant-fact()
+end_variable
+begin_variable
+var5
+-1
+3
+Atom holding(a)
+Atom on(a, b)
+Atom ontable(a)
+end_variable
+begin_variable
+var6
+-1
+2
+Atom clear(a)
+Atom irrelevant-fact()
+end_variable
+begin_variable
+var7
+-1
+5
 Atom plan-pos-0()
 Atom plan-pos-1()
 Atom plan-pos-2()
+Atom plan-pos-3()
+Atom plan-pos-4()
 end_variable
-0
+4
+begin_mutex_group
+2
+5 0
+6 0
+end_mutex_group
+begin_mutex_group
+2
+0 0
+5 1
+end_mutex_group
+begin_mutex_group
+2
+2 0
+3 0
+end_mutex_group
+begin_mutex_group
+3
+2 0
+4 0
+5 0
+end_mutex_group
 begin_state
+0
+0
+1
+0
+0
+2
 0
 0
 end_state
 begin_goal
-2
-0 2
-1 2
+3
+5 1
+6 0
+7 4
 end_goal
-4
+8
 begin_operator
-get_stick stick1
+pick-up a
 0
-2
-0 0 0 1
-0 1 0 1
+4
+0 4 0 1
+0 5 2 0
+0 6 0 1
+0 7 2 3
 1
 end_operator
 begin_operator
-make_axe stick1
+pick-up c
 0
-2
-0 0 1 2
-0 1 1 2
+4
+0 2 1 0
+0 3 0 1
+0 4 0 1
+0 7 0 1
 1
 end_operator
 begin_operator
 skip-action plan-pos-0
 0
 1
-0 1 0 1
+0 7 0 1
 0
 end_operator
 begin_operator
 skip-action plan-pos-1
 0
 1
-0 1 1 2
+0 7 1 2
 0
+end_operator
+begin_operator
+skip-action plan-pos-2
+0
+1
+0 7 2 3
+0
+end_operator
+begin_operator
+skip-action plan-pos-3
+0
+1
+0 7 3 4
+0
+end_operator
+begin_operator
+stack a b
+0
+5
+0 0 0 1
+0 4 -1 0
+0 5 0 1
+0 6 -1 0
+0 7 3 4
+1
+end_operator
+begin_operator
+stack c d
+0
+5
+0 2 0 2
+0 3 -1 0
+0 1 0 1
+0 4 -1 0
+0 7 1 2
+1
 end_operator
 0
--- src/translate/finding_causal_links_plan.py
+++ src/translate/finding_causal_links_plan.py
@@ -57,14 +57,15 @@ def extracting_causal_links(init_values_list,final_precond_effects_list):
     list_final2=[]
     for i in range(len(list_final)):
         elem = list_final[i]
+        fact=elem[1]
         producers = elem[0]
         consumers = elem[2]
-        for j in range(len(producers)):
-            if j < len(consumers):
-                list_final2.append((producers[j], elem[1], consumers[j]))
-            else:
-                list_final2.append((producers[j], elem[1], -1))
-
+        if "plan-pos" not in fact and "irrelevant-fact" not in fact: # esto es solo cdo llamo a python3 finding_causal_links_plan.py -t action-elimination.sas -p1 sas_plan -p2 ../../domains/blocks/sas_plan.2 --subsequence
+            for j in range(len(producers)):
+                if j < len(consumers):
+                    list_final2.append((producers[j], elem[1], consumers[j]))
+                else:
+                    list_final2.append((producers[j], elem[1], -1))
     return list_final2
         
 
@@ -122,8 +123,31 @@ def main():
 
     final_precond_effects_list = list(zip(operators_list, precond_actions_list, effects_actions_list))
 
-    list_causal_links = extracting_causal_links(init_values_list,final_precond_effects_list)
-    print(list_causal_links)
+    list_causal_links_ae = extracting_causal_links(init_values_list,final_precond_effects_list)
+    print(list_causal_links_ae)
+    #tengo que hacer 2 llamadas, una para obtener los causal links del paln con las skip actios y otro del plan normal
+
+    #suponiendo aque aquí  obtengo los casusal links del plan normal:
+    list_causal_links_plan=[(0, 'Atom ontable(c)', 1), (0, 'Atom ontable(d)', -1), (0, 'Atom clear(c)', 1), (2, 'Atom clear(c)', -1), (0, 'Atom clear(d)', 2), (0, 'Atom clear(b)', 4), (0, 'Atom handempty()', 1), (2, 'Atom handempty()', 3), (4, 'Atom handempty()', -1), (0, 'Atom ontable(b)', -1), (0, 'Atom ontable(a)', 3), (0, 'Atom clear(a)', 3), (4, 'Atom clear(a)', -1), (1, 'NegatedAtom clear(c)', 2), (1, 'NegatedAtom handempty()', 2), (3, 'NegatedAtom handempty()', 4), (1, 'Atom holding(c)', 2), (2, 'NegatedAtom clear(d)', -1), (2, 'Atom on(c, d)', -1), (3, 'NegatedAtom clear(a)', 4), (3, 'Atom holding(a)', 4), (4, 'NegatedAtom clear(b)', -1), (4, 'Atom on(a, b)', -1)]
+
+    diccionario = {}
+    for elem in list_causal_links_plan:
+        clave = elem[2]
+        valor = (elem[0],elem[1])
+        if clave in diccionario:
+            diccionario[clave].append(valor)
+        else:
+            diccionario[clave] = [valor]
+    diccionario_ordenado = dict(sorted(diccionario.items()))    
+    print(diccionario_ordenado)
+
+    for element in list_causal_links_ae:
+        if element not in list_causal_links_plan and element[2]!=-1:
+            print(element)
+
+
+
+
 
     #aqui tengo que determinar si el plan esta perfectamente justificado o no. En caso negativo hacer lo de abajo, sino sacar un mensaje de que está perfect justif y que no hay acciones irr
     
@@ -139,24 +163,24 @@ def main():
     #list_causal_links = [(0, 'Atom ontable(c)', 1), (0, 'Atom ontable(d)', -1), (0, 'Atom clear(c)', 1), (2, 'Atom clear(c)', -1), (0, 'Atom clear(d)', 2), (0, 'Atom clear(b)', 4), (0, 'Atom handempty()', 1), (2, 'Atom handempty()', -1), (4, 'Atom handempty()', -1), (0, 'Atom ontable(b)', -1), (0, 'Atom ontable(a)', 3), (0, 'Atom clear(a)', 3), (4, 'Atom clear(a)', -1), (1, 'NegatedAtom clear(c)', -1), (1, 'NegatedAtom handempty()', -1), (3, 'NegatedAtom handempty()', 4), (1, 'Atom holding(c)', -1), (2, 'NegatedAtom clear(d)', -1), (2, 'Atom on(c, d)', -1), (3, 'NegatedAtom clear(a)', 4), (3, 'Atom holding(a)', 4), (4, 'NegatedAtom clear(b)', -1), (4, 'Atom on(a, b)', -1)]
    
           
-    for i in range(len(plan1)):
-        todos_menos_uno = True
-        irr = False
-        temp_list = []
+    # for i in range(len(plan1)):
+    #     todos_menos_uno = True
+    #     irr = False
+    #     temp_list = []
         
-        for j in range(len(list_causal_links)):
-            elemento = list_causal_links[j]
-            if elemento[0] == i and i in list_pos_irr_actions:
-                irr = True
-                temp_list.append(elemento)
+    #     for j in range(len(list_causal_links)):
+    #         elemento = list_causal_links[j]
+    #         if elemento[0] == i and i in list_pos_irr_actions:
+    #             irr = True
+    #             temp_list.append(elemento)
 
-        for elemento in temp_list:
-            if elemento[2] != -1:
-                todos_menos_uno = False
-                break
+    #     for elemento in temp_list:
+    #         if elemento[2] != -1:
+    #             todos_menos_uno = False
+    #             break
 
-        if todos_menos_uno and irr:
-            print(f"Action #{i}", plan1[i], " is irrelevant since it produces facts that are not consumed by anyone.")
+    #     if todos_menos_uno and irr:
+    #         print(f"Action #{i}", plan1[i], " is irrelevant since it produces facts that are not consumed by anyone.")
           
 
 
